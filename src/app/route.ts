e005d4b55ae1e614de1b040cfe9423cc23884619
adityaa72 <adityah1357908642@gmail.com>
2024-05-09 21:33:36 +0530

    fix rout

--- src/app/route.ts
+++ src/app/route.ts
@@ -23,7 +23,7 @@ export async function GET(req: NextRequest) {
     where: {
       userId: Number(userId),
       Staff: {
-        isActive: false,
+        isActive: true,
       },
     },
   });
